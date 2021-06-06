58322ac3be7c2308fc3558f13236168f8cace986
Arpitkumar Soni <50675925+arpit2304@users.noreply.github.com>
2021-06-06 09:01:30 -0400

    Update Game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -10,8 +10,6 @@ import java.util.ArrayList;
 /**
  * The class that models your game. You should create a more specific
  * child of this class and instantiate the methods given.
- *
- * @author dancye, 2018
  */
 public abstract class Game
 {
